c3d07e8b118820aa08a97f868d20dc09c8af8f95
Stoycho Kostov <stoichokostov@gmail.com>
2022-01-19 00:08:23 -0600

    http header based versioning

--- my-books/Controllers/v1/TestController.cs
+++ my-books/Controllers/v1/TestController.cs
@@ -4,15 +4,29 @@ using Microsoft.AspNetCore.Mvc;
 namespace my_books.Controllers.v1
 {
     [ApiVersion("1.0")]
-    //[Route("api/[controller]")]
-    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.2")]
+    [ApiVersion("1.9")]
+    [Route("api/[controller]")]
+    //[Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
     {
         [HttpGet("get-test-data")]
         public IActionResult Get()
         {
-            return Ok("This is a test controller v1.");
+            return Ok("This is a test controller v1.0.");
+        }
+
+        [HttpGet("get-test-data"), MapToApiVersion("1.2")]
+        public IActionResult GetV12()
+        {
+            return Ok("This is a test controller v1.2.");
+        }
+
+        [HttpGet("get-test-data"), MapToApiVersion("1.9")]
+        public IActionResult GetV19()
+        {
+            return Ok("This is a test controller v1.9.");
         }
     }
 }
--- my-books/Controllers/v2/TestController.cs
+++ my-books/Controllers/v2/TestController.cs
@@ -4,8 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace my_books.Controllers.v2
 {
     [ApiVersion("2.0")]
-    //[Route("api/[controller]")]
-    [Route("api/v{version:apiVersion}/[controller]")]
+    [Route("api/[controller]")]
+    //[Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
     {
