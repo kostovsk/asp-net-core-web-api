1140012db88a784168a2063e899f8379d91ed9c3
Stoycho Kostov <stoichokostov@gmail.com>
2022-01-20 00:13:27 -0600

    add log table using efcore migration

--- my-books/Data/AppDbContext.cs
+++ my-books/Data/AppDbContext.cs
@@ -21,11 +21,14 @@ namespace my_books.Data
                 .HasOne(a => a.Author)
                 .WithMany(b => b.Book_Authors)
                 .HasForeignKey(c => c.AuthorId);
+
+            modelBuilder.Entity<Log>().HasKey(n => n.Id);
         }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book_Author> Books_Auhtors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Log> Logs { get; set; }
     }
 }
