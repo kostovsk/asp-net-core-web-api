fcdc9bc438aae784895c919b081e9d8f60f6380f
Stoycho Kostov <stoichokostov@gmail.com>
2022-01-20 23:26:11 -0600

    retrieve logs from db

---
+++ my-books/Controllers/LogsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        private LogsService _logsService;
+        public LogsController(LogsService logsService)
+        {
+            _logsService = logsService;
+        }
+
+        [HttpGet("get-all-logs-from-db")]
+        public IActionResult GetAllLogsFromDB()
+        {
+            try
+            {
+                var allLogs = _logsService.GetAllLogsFromDB();
+                return Ok(allLogs);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest("Could not load logs from DB");
+            }
+        }
+    }
+}
